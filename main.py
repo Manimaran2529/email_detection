83e0941fa1a1ce63dc0a26eaadf17100d64a519c
manimaran <manimaranh9965@gmail.com>
2026-01-27 15:19:06 +0530

    email

--- main.py
+++ main.py
@@ -1,45 +1,35 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report
-mail=pd.read_csv("email_spam.csv")
-mail.drop_duplicates(inplace=True)
-#mail_total=mail["type"].value_counts()
-#plt.pie(mail_total,autopct="%1.1f%%")
+mail=pd.read_csv("spam.csv")
+mail.drop_duplicates(inplace=True,subset='Message')
+mail_total=mail["Category"].value_counts()
+
+#plt.pie(mail_total, labels=["notspam","spam"],autopct="%1.1f%%")
 #plt.show()
+lb=LabelEncoder()
+mail["Category"]=lb.fit_transform(mail["Category"])
+print(mail.dtypes)
 
-mail["type"]=mail["type"].map({"spam":0,"not spam":1})
-#print(mail.dtypes)
-mail["combined_text"] = mail["title"] + " " + mail["text"]
-
-
-
-x=mail["combined_text"]
-y=mail["type"]
-X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
-
-tf_x=TfidfVectorizer(lowercase=True,stop_words="english",ngram_range=(1, 2))
+x=mail["Message"]
+y=mail["Category"]
 
 
-X_train_tfidf=tf_x.fit_transform(X_train)
-X_test_tfidf=tf_x.transform(X_test)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 
-lr=LogisticRegression(max_iter=1000,
-                       class_weight="balanced")
-lr.fit(X_train_tfidf,y_train)
+tf=TfidfVectorizer(lowercase=True,stop_words="english")
 
-y_predict=lr.predict(X_test_tfidf)
+x_train_tf=tf.fit_transform(x_train)
+x_test_tf=tf.transform(x_test)
 
-print("score",classification_report(y_test,y_predict))
+lr=LinearSVC()
 
-a = input("Enter your mail: ")
+lr.fit(x_train_tf,y_train)
 
-b = tf_x.transform([a])
-c_predict = lr.predict(b)
+y_pre=lr.predict(x_test_tf)
 
-if c_predict[0] == 0:
-    print("🚨 This mail is SPAM")
-else:
-    print("✅ This mail is NOT SPAM")
+print("report",classification_report(y_test,y_pre))
