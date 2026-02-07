c0edfd2dbe6be0de726049c5743699f378e95f91
manimaran <manimaranh9965@gmail.com>
2026-02-07 21:47:53 +0530

    second day

--- mm.py
+++ mm.py
@@ -4,4 +4,4 @@ mm=pd.read_csv("scam_dataset.csv")
 mm["message"]=mm["message"].str.lower()
 mm["message"]=mm["message"].apply (lambda x:  re.sub(r"[^a-z\s]", "", x))
 
-print(mm["message"].head(10))
+print(mm["message"].head(10)
