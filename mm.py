8f4b6e90e11f7099e00918d2e19e5ad4af62f5e4
manimaran <manimaranh9965@gmail.com>
2026-02-08 12:33:47 +0530

    nlp

--- mm.py
+++ mm.py
@@ -1,7 +1,10 @@
-import re
 import pandas as pd
+import re
 mm=pd.read_csv("scam_dataset.csv")
 mm["message"]=mm["message"].str.lower()
-mm["message"]=mm["message"].apply(lambda x:re.sub(r"[^a-z\s]","", x))
+mm["message"]=mm["message"].apply(
+    lambda x : re.sub(r"[^a-z\s]","",x)
+)
 mm["message"]=mm["message"].str.split()
+mm["message"]=mm["message"].str.stopword()
 print(mm["message"].head(10))
